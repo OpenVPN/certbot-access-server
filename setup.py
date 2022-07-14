bca833bbbe19bd25e22958155a0d502b8de01271
Kostiantyn Panfilov <kostiantyn.panfilov@openvpn.net>
2022-07-14 16:14:00 -0400

    Adjust installation files

--- setup.py
+++ setup.py
@@ -3,26 +3,13 @@ import sys
 
 from setuptools import setup, find_packages
 
-version = '1.26.0.dev0'
+version = '1.0.0'
 
 install_requires = [
-    'setuptools>=41.6.0',
+    'setuptools>=39.0.0',
+    'certbot>=1.20.0',
+    'acme>=1.20.0'
 ]
-
-
-if not os.environ.get('SNAP_BUILD'):
-    install_requires.extend([
-        # We specify the minimum acme and certbot version as the current plugin
-        # version for simplicity. See
-        # https://github.com/certbot/certbot/issues/8761 for more info.
-        f'acme>={version}',
-        f'certbot>={version}',
-    ])
-elif 'bdist_wheel' in sys.argv[1:]:
-    raise RuntimeError('Unset SNAP_BUILD when building wheels '
-                       'to include certbot dependencies.')
-if os.environ.get('SNAP_BUILD'):
-    install_requires.append('packaging')
 
 docs_extras = [
     'Sphinx>=1.0',  # autodoc_member_order = 'bysource', autodoc_default_flags
@@ -30,7 +17,7 @@ docs_extras = [
 ]
 
 setup(
-    name='certbot-plugin-accessserver',
+    name='certbot-access-server',
     version=version,
     description="OpenVPN Access Server Installer plugin for Certbot",
     url='https://github.com/certbot/certbot',
